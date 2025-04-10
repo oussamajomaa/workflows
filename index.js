dcd04ecc3d37ccde24e44ca15188faf8303149cd
oussamajomaa <osm70@gmx.com>
2025-04-10 10:55:55 +0200

    ajouter une sconde route

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get('/', (req,res)=> {
     res.json('Heloo from Workflows Gitactions')
 })
 
+app.get('/user', (req,res)=> {
+    res.json('Voici une liste des utilisateurs')
+})
+
 app.listen('3001', ()=> console.log('Server is running on 3001'))
